ce04a533085e2fd0c1d39dfb7b5477e0b53dc83d
Steve <steve.vernyi@gmail.com>
2020-02-03 01:40:47 -0500

    Implements meta changes in correct area

--- SvenAiWeb/nuxt.config.js
+++ SvenAiWeb/nuxt.config.js
@@ -6,12 +6,12 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s | Sven.ai',
+    title: 'Home',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Professional/portfolio website for Stephen Vernyi (Sven)' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
