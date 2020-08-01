a9e1a46aa020c9cd6d836e37f449a9599fc0b025
Steve <steve.vernyi@gmail.com>
2020-08-01 01:30:43 -0400

    Attempts to use GitVersion for docker image versions

--- SvenAiWeb/pages/index.vue
+++ SvenAiWeb/pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <v-row align="center" justify="center">
-    <v-col cols="2"><p class="text-center">Test</p></v-col>
+    <v-col cols="2">
+      <p class="text-center">
+        Test
+      </p>
+    </v-col>
   </v-row>
 </template>
 
