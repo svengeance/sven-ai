fd1035401e4fd6e78030cd0395ad60be6e7cf0ec
Sven <sven.vernyi@gmail.com>
2020-08-24 03:24:05 -0400

    Adds FavIcon

--- SvenAiWeb/nuxt.config.js
+++ SvenAiWeb/nuxt.config.js
@@ -27,7 +27,11 @@ export default {
       { hid: 'description', name: 'description', content: 'Professional/portfolio website for Stephen Vernyi (Sven)' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon/apple-touch-icon.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon/favicon-16x16.png" },
+      { rel: "manifest", href: "/favicon/site.webmanifest" }
     ]
   },
   /*
