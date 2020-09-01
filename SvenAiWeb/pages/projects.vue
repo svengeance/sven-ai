0a03fe9c66704f0b9fe8cd89496b69eccf11fae4
svengeance <steve.vernyi@gmail.com>
2020-09-01 00:39:49 -0400

    Abandons hope of 3'rd party libraries in favor of manual event handling

--- SvenAiWeb/pages/projects.vue
+++ SvenAiWeb/pages/projects.vue
@@ -14,7 +14,7 @@
           </v-list-item-content>
         </v-list-item>
 
-        <div v-touch:moving.disablePassive.stop="test" v-if="isFlipped(index)" class="pt-4 card-text-container">
+        <div @touchmove.stop v-if="isFlipped(index)" class="pt-4 card-text-container">
           <perfect-scrollbar :options="cardScrollOptions">
             <v-card-text v-html="project.description" class="card-text" />
           </perfect-scrollbar>
@@ -175,13 +175,6 @@ export default {
     }
   },
   methods: {
-    test(args) {
-      args.cancelBubble = true
-      args.stopImmediatePropagation()
-      args.stopPropagation()
-      args.preventDefault()
-      console.log(args)
-    },
     flipCard(cardNumber) {
       const existingIndex = this.flippedCards.indexOf(cardNumber)
       if (existingIndex >= 0)
