72324b77e686b81ac2d414a6ce3117717296022d
svengeance <steve.vernyi@gmail.com>
2021-05-03 00:02:13 -0400

    Fix log spam - thanks Nuxt

--- SvenAiWeb/nuxt.config.js
+++ SvenAiWeb/nuxt.config.js
@@ -172,19 +172,10 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    // extend (config, ctx) {
-    //   if (ctx.isDev && ctx.isClient) {
-    //     config.module.rules.push({
-    //       enforce: 'pre',
-    //       test: /\.(js|vue)$/,
-    //       loader: 'eslint-loader',
-    //       exclude: /(node_modules)/,
-    //       options: {
-    //         fix: true
-    //       }
-    //     })
-    //   }
-    // },
-    // analyze: true
+    babel:{
+      plugins: [
+        ['@babel/plugin-proposal-private-methods', { loose: true }]
+      ]
+    }
   }
 }
